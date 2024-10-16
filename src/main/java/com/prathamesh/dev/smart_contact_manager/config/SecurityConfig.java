0de09b56e3a7332b40c904b5871c32b5d77a6c6a
prathamesh4545 <dateprathamesh650@gmail.com>
2024-10-16 23:08:59 +0530

    initial security configration complete

--- src/main/java/com/prathamesh/dev/smart_contact_manager/config/SecurityConfig.java
+++ src/main/java/com/prathamesh/dev/smart_contact_manager/config/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.prathamesh.dev.smart_contact_manager.config;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.prathamesh.dev.smart_contact_manager.Service.SecurityCustomUserDetailService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -38,8 +49,40 @@ public class SecurityConfig {
         });
 
         // form default login
-        http.formLogin(Customizer.withDefaults());
+        // http.formLogin(Customizer.withDefaults());
 
+        http.formLogin(formLogin -> {
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+            formLogin.failureForwardUrl("/login?error=true");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+            //     @Override
+            //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            //             AuthenticationException exception) throws IOException, ServletException {
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+            //     }
+                
+            // });
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            //     @Override
+            //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            //             Authentication authentication) throws IOException, ServletException {
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+            //     }
+                
+            // });
+        });
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.logout(logoutform -> {
+            logoutform.logoutUrl("/logout");
+            logoutform.logoutSuccessUrl("/login?logout=true");
+        });
         return http.build();
     }
 
