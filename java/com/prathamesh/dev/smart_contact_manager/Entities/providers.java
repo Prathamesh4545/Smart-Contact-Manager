5f9bbe186f39c8dbcdefb525bcc3e3aedd5c9739
Prathamesh date <102506898+Prathamesh4545@users.noreply.github.com>
2024-10-18 00:37:24 +0530

    Delete java/com/prathamesh/dev/smart_contact_manager directory

--- java/com/prathamesh/dev/smart_contact_manager/Entities/providers.java
+++
@@ -1,5 +0,0 @@
-package com.prathamesh.dev.smart_contact_manager.Entities;
-
-public enum providers {
-    SELF,GOOGLE,GITHUB
-}
--- java/com/prathamesh/dev/smart_contact_manager/Helper/MessageType.java
+++
@@ -1,5 +0,0 @@
-package com.prathamesh.dev.smart_contact_manager.Helper;
-
-public enum MessageType {
-    blue, red, green, yellow
-}
